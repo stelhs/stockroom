f69159bf023613700fe9319f84091b33c1829f39
xroo <xroo@google.com>
2020-06-18 00:01:59 +0300

    update SQL tables structute

--- private/db_struct.sql
+++ private/db_struct.sql
@@ -1,45 +1,47 @@
+-- MySQL dump 10.13  Distrib 5.7.23, for Linux (x86_64)
+--
+-- Host: localhost    Database: things
+-- ------------------------------------------------------
+-- Server version	5.7.23-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Структура таблицы `catalog`
+-- Table structure for table `catalog`
 --
 
+DROP TABLE IF EXISTS `catalog`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `catalog` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `parent_id` int(11) NOT NULL,
   `name` varchar(512) NOT NULL,
   `description` text,
   `user_id` int(11) NOT NULL,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=426 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Индексы сохранённых таблиц
+-- Table structure for table `images`
 --
 
---
--- Индексы таблицы `catalog`
---
-ALTER TABLE `catalog`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `catalog`
---
-ALTER TABLE `catalog`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-
-
---
--- Структура таблицы `images`
---
-
+DROP TABLE IF EXISTS `images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `images` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `obj_type` enum('objects','locations') NOT NULL,
   `obj_id` int(11) NOT NULL,
   `name` varchar(50) DEFAULT NULL,
@@ -47,142 +49,87 @@ CREATE TABLE `images` (
   `hash` varchar(500) DEFAULT NULL,
   `filename` varchar(500) DEFAULT NULL,
   `extension` varchar(8) NOT NULL,
-  `width` int(10) UNSIGNED DEFAULT NULL,
-  `height` int(10) UNSIGNED DEFAULT NULL,
+  `width` int(10) unsigned DEFAULT NULL,
+  `height` int(10) unsigned DEFAULT NULL,
   `original_filename` varchar(50) DEFAULT NULL,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12267 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Индексы сохранённых таблиц
+-- Table structure for table `location`
 --
 
---
--- Индексы таблицы `images`
---
-ALTER TABLE `images`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `images`
---
-ALTER TABLE `images`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
-
-
---
--- Структура таблицы `location`
---
-
+DROP TABLE IF EXISTS `location`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `location` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `parent_id` int(11) NOT NULL,
   `name` varchar(512) NOT NULL,
   `description` text,
-  `fullness` int(11) NOT NULL,
-  `is_box` tinyint(1) NOT NULL DEFAULT '0',
-  `is_absent` tinyint(1) NOT NULL DEFAULT '0',
+  `size1` int(11) NOT NULL COMMENT 'ширина или высота или глубина',
+  `size2` int(11) NOT NULL COMMENT 'ширина или высота или глубина',
+  `size3` int(11) NOT NULL COMMENT 'ширина или высота или глубина',
+  `fullness` int(11) NOT NULL COMMENT 'заполненность ячейки в процентах',
+  `volume` bigint(20) NOT NULL COMMENT 'объём ячейки в милилитрах',
+  `free_volume` bigint(20) NOT NULL COMMENT 'свободный объём в милилитрах',
+  `is_box` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'локация является ячейкой или нет',
+  `is_absent` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'стоит в true если ячейка отсутсвует',
   `user_id` int(11) NOT NULL,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='nodes tree';
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=378 DEFAULT CHARSET=utf8 COMMENT='nodes tree';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Индексы сохранённых таблиц
+-- Table structure for table `objects`
 --
 
---
--- Индексы таблицы `location`
---
-ALTER TABLE `location`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `location`
---
-ALTER TABLE `location`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-
-
-
---
--- Структура таблицы `objects`
---
-
+DROP TABLE IF EXISTS `objects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `objects` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(512) NOT NULL,
   `description` text,
-  `number` int(11) NOT NULL DEFAULT '1',
+  `attrs` text NOT NULL COMMENT 'атрибуты. дополнительные параметры в формате ключ:значение с переносом строк',
+  `number` int(11) NOT NULL DEFAULT '1' COMMENT 'количество предметов',
   `catalog_id` int(11) NOT NULL,
   `location_id` int(11) NOT NULL,
-  `absent` int(1) NOT NULL DEFAULT '0',
+  `absent` int(1) NOT NULL DEFAULT '0' COMMENT 'выставляется в true если предмет отсутсвует на своём месте',
   `user_id` int(11) NOT NULL,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3043 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Индексы сохранённых таблиц
+-- Table structure for table `users`
 --
 
---
--- Индексы таблицы `objects`
---
-ALTER TABLE `objects`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `objects`
---
-ALTER TABLE `objects`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-
-
---
--- Структура таблицы `users`
---
-
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `login` varchar(512) NOT NULL,
   `pass` varchar(512) NOT NULL,
   `hash` varchar(512) NOT NULL,
   `role` enum('admin','viewer') NOT NULL,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Индексы сохранённых таблиц
---
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
---
--- Индексы таблицы `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-  
+-- Dump completed on 2020-06-18  0:01:24
